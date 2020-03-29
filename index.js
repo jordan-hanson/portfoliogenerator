fa00d196928763cdd40c8beee406bab1f3b7dd83
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-03-28 19:46:38 -0600

    github profile pic and h1 added

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 var URL = `https://api.github.com/users/${"jordan-hanson"}?client_id=f5285491670655d3954f&client_secret=o913d6b039b0bf45a4c004f2e61726dec4d3c9db1`
 var axios = require("axios")
 var fs = require("fs")
+var inquirer = require("inquirer")
+var generateHtml = require("./generateHTML")
+
 axios.get(URL).then(function (gitHubData) {
     console.log(gitHubData.data)
-    var HTML =
-        `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>${gitHubData.data.login}</h1>
-    </body>
-    </html>`
+    gitHubData.data.color = "pink"
+    generateHtml(gitHubData.data)
 
-    fs.writeFile("portfolio.html", HTML, function (error) {
-        console.log(error)
-    })
 })
