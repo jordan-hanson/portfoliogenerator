449dd2bfb38b91a8a9f40fdf6a9f4ae167e02002
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-03-29 21:49:18 -0600

    working prompt function in async function and adding variables to pull from the answers

--- index.js
+++ index.js
@@ -1,43 +1,113 @@
-// const URL = `https://api.github.com/users/${"jordan-hanson"}?client_id=f5285491670655d3954f&client_secret=o913d6b039b0bf45a4c004f2e61726dec4d3c9db1`
+const URL = `https://api.github.com/users/${"username"}`
+const starUrl = `https://api.github.com/users/${"username"}/starred`
 const axios = require("axios")
 const fs = require("fs")
 const inquirer = require("inquirer")
-// const generateHtml = require("./generateHTML")
+const generateHtml = require("./generateHTML")
 
-// axios.get(URL).then(function (gitHubData) {
-//     console.log(gitHubData.data)
-//     gitHubData.data.color = "pink"
-//     generateHtml(gitHubData.data)
-
-// })
 async function data() {
-    const profileData = await
-        inquirer
-            .prompt([
+    function profilePrompt() {
+        let profileData =
+            inquirer
+                .prompt([
 
-                {
-                    type: "input",
-                    name: "username",
-                    message: "What is your GitHub username?",
-                },
+                    {
+                        type: "input",
+                        name: "username", //ANSWER
+                        message: "What is your GitHub username?",
+                    },
 
-                {
-                    type: "list",
-                    name: "color",
-                    message: "What is your favorite color?",
-                    choices: [
-                        'green',
-                        'blue',
-                        'pink',
-                        'red'
-                    ]
-                }
+                    {
+                        type: "list",
+                        name: "color", // ANSWER
+                        message: "What is your favorite color?",
+                        choices: [
+                            'green',
+                            'blue',
+                            'pink',
+                            'red'
+                        ]
+                    }
 
-            ]).then(answers => {
-                console.log("my answers worked", answers)
-            })
+                ]).then(answers => {
+                    console.log("my answers worked", answers)
+                })
+
+    }
+    profilePrompt();
+    let name = await profilePrompt.username
+    console.log("this is the name", name)
+    let color = await profilePrompt.color
+    console.log("this is the color choice", color)
+    // await axios.get(URL).then(function (gitHubData) {
+    //     console.log(gitHubData.data)
+    //     // gitHubData.data.color = "color"
+    //     generateHtml(gitHubData.data)
+
 }
 data();
+//
+
+// THIS CODE WORKS AND WILL PROMPT THE USER BUT DOESNT UPDATE GENERATE HTML
+// async function data() {
+//     const profileData = await
+//         inquirer
+//             .prompt([
+
+//                 {
+//                     type: "input",
+//                     name: "username",
+//                     message: "What is your GitHub username?",
+//                 },
+
+//                 {
+//                     type: "list",
+//                     name: "color",
+//                     message: "What is your favorite color?",
+//                     choices: [
+//                         'green',
+//                         'blue',
+//                         'pink',
+//                         'red'
+//                     ]
+//                 }
+
+//             ]).then(answers => {
+//                 console.log("my answers worked", answers)
+//             })
+// }
+// data();
+
+// function name() {
+//     let name = inquirer
+//         .prompt(
+//             {
+//                 type: "input",
+//                 name: "Name",
+//                 message: "What is your first and last name?",
+//             })
+//     return name
+// }
+// // console.log("this is the name", name)
 
 
+// function color() {
+//     let color = inquirer
+//         .prompt(
+//             {
+//                 type: "list",
+//                 name: "color",
+//                 message: "What is your favorite color?",
+//                 choices: [
+//                     'green',
+//                     'blue',
+//                     'pink',
+//                     'red']
 
+//             })
+//     return color
+// }
+// // console.log("this is the color", color)
+
+// name((console.log("this is the name", name)));
+// color((console.log("this is the color", color)));
